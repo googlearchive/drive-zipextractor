f6b493465c8a697e657e73fc1232839a7e9cd3d3
Mike Procopio <mprocopio@gmail.com>
2014-01-16 03:33:28 -0700

    Update client id

--- js/zipextractor/config.js
+++ js/zipextractor/config.js
@@ -1,5 +1,5 @@
 zipextractor.config.DRIVE_API_CONFIG_DATA = {
-    'clientId': '824911851129-01akc4k0noqt42n803nbtvoolaugf87j.apps.googleusercontent.com',
+    'clientId': '824911851129-k13qd3ah93spnmrhcqj3pfkaeaua3g13.apps.googleusercontent.com',
     'appId' :'824911851129',    
     'scopes': ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive.install'],
     'apiKey': 'AIzaSyBW4IYnFZLyPFFKtZ2qWJYTlURyyE-Vt_4' 
