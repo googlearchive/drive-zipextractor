ce423747bef62efca218d96ed6de99a8c2de953e
Mike Procopio <mprocopio@gmail.com>
2014-01-20 21:39:07 -0700

    Update client ID for recent devconsole updates.

--- js/zipextractor/config.js
+++ js/zipextractor/config.js
@@ -1,5 +1,5 @@
 zipextractor.config.DRIVE_API_CONFIG_DATA = {
-    'clientId': '824911851129-k13qd3ah93spnmrhcqj3pfkaeaua3g13.apps.googleusercontent.com',
+    'clientId': '824911851129-4r4jtv80tf2ebk5sp868p64glp7lmbol.apps.googleusercontent.com',
     'appId' :'824911851129',    
     'scopes': ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive.install'],
     'apiKey': 'AIzaSyBW4IYnFZLyPFFKtZ2qWJYTlURyyE-Vt_4' 
