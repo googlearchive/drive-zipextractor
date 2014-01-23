bd13a115f03c99c193ccea726694dd35c4b6d521
Mike Procopio <mprocopio@gmail.com>
2014-01-23 09:01:35 -0700

    Add License (Apache V2) and copyright notice.

--- js/zipextractor/config.js
+++ js/zipextractor/config.js
@@ -1,3 +1,18 @@
+// Copyright 2014 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
 zipextractor.config.DRIVE_API_CONFIG_DATA = {
     'clientId': '824911851129-4r4jtv80tf2ebk5sp868p64glp7lmbol.apps.googleusercontent.com',
     'appId' :'824911851129',    
--- js/zipextractor/mimetypes.js
+++ js/zipextractor/mimetypes.js
@@ -1,3 +1,4 @@
+
 function getMimeTypes() {
     return {
                 // Extensions:
