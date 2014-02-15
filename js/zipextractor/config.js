e5375f2dd9268d6d4be4e19596d60a71aac743b2
Mike Procopio <mprocopio@gmail.com>
2014-02-15 08:17:48 -0700

    (v0.95) Update Client ID for recent devconsole internationalization

--- js/zipextractor/config.js
+++ js/zipextractor/config.js
@@ -14,7 +14,7 @@
 
 
 zipextractor.config.DRIVE_API_CONFIG_DATA = {
-    'clientId': '824911851129-4r4jtv80tf2ebk5sp868p64glp7lmbol.apps.googleusercontent.com',
+    'clientId': '824911851129-6d64j0e08s86ih74l0lu386tqqfv71t9.apps.googleusercontent.com',
     'appId' :'824911851129',    
     'scopes': ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive.install'],
     'apiKey': 'AIzaSyBW4IYnFZLyPFFKtZ2qWJYTlURyyE-Vt_4' 
